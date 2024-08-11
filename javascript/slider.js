173f285e65e8277342bc2cd200fdaadc25cb8abb
alsaour <proalsaour@gmail.com>
2024-08-11 22:07:34 +0200

    ads section

---
+++ javascript/slider.js
@@ -0,0 +1,18 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    autoplay: {
+      delay: 2000, // مدة التأخير بالميلي ثانية (2000 ميلي ثانية = 2 ثانية)
+      disableOnInteraction: false, // استمرار التشغيل التلقائي حتى عند التفاعل مع السلايدر
+    },
+    speed: 1000
+  });
