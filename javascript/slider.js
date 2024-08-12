93b3d452f33aea36c1a52fbebdbdf4371ad13a44
alsaour <proalsaour@gmail.com>
2024-08-12 21:34:47 +0200

    ads nav & brands section

--- javascript/slider.js
+++ javascript/slider.js
@@ -14,5 +14,21 @@ var TrandingSlider = new Swiper('.tranding-slider', {
       delay: 2000, // مدة التأخير بالميلي ثانية (2000 ميلي ثانية = 2 ثانية)
       disableOnInteraction: false, // استمرار التشغيل التلقائي حتى عند التفاعل مع السلايدر
     },
-    speed: 1000
+    speed: 1000,
+    loopAdditionalSlides: 30, // لجعل الحركة أكثر سلاسة
+    freeMode: true, // تفعيل الوضع الحر لضمان الحركة المستمرة
+
+  });
+var BrandingSlider = new Swiper('.branding-slider', {
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+    autoplay: {
+      delay:0,
+      disableOnInteraction: true, // استمرار التشغيل التلقائي حتى عند التفاعل مع السلايدر
+    },
+    speed: 10000,
+    loopAdditionalSlides: 30, // لجعل الحركة أكثر سلاسة
+    freeMode: true, // تفعيل الوضع الحر لضمان الحركة المستمرة
   });
